4ae00fd8849c9d530fa0ca3279587b725cc92cb2
Frank <jssmith883@gmail.com>
2022-04-27 01:16:14 +0300

    Define a custom HashTable class

--- test_hashtable.py
+++ test_hashtable.py
@@ -1,3 +1,4 @@
+from hashtable import HashTable
 
 """
 Test case should be as independent and atomic as possible
@@ -11,4 +12,11 @@ given-when-then convention
 def test_should_always_pass():
     assert 2 + 2 == 4, "This is just a dummy test"
 
+def test_should_create_hashtable():
+    assert HashTable(capacity=100) is not None
 
+def test_should_report_capacity():
+    assert len(HashTable(capacity=10)) == 10
+
+def test_should_create_empty_value_slots():
+    assert HashTable(capacity=3).values == [None, None, None]
