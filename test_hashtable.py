c53150503f626b84ffe4c961a3c79a7cc6ee61cc
Frank <jssmith883@gmail.com>
2022-04-27 01:15:09 +0300

    Take a crash course in TDD

---
+++ test_hashtable.py
@@ -0,0 +1,14 @@
+
+"""
+Test case should be as independent and atomic as possible
+
+given-when-then convention
+    # Given
+    # When
+    # Then
+"""
+
+def test_should_always_pass():
+    assert 2 + 2 == 4, "This is just a dummy test"
+
+
