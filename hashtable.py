ea160502261b2d15f50abf45447276b192e55b03
Frank <jssmith883@gmail.com>
2022-04-28 00:33:15 +0300

    Insert a Key-Value Pair

--- hashtable.py
+++ hashtable.py
@@ -1,8 +1,14 @@
 
+BLANK = object()
+
 class HashTable:
     
     def __init__(self, capacity):
-        self.values = capacity * [None]
+        self.values = capacity * [BLANK]
     
     def __len__(self):
         return len(self.values)
+
+    def __setitem__(self, key, value):
+        index = hash(key) % len(self)
+        self.values[index] = value
--- test_hashtable.py
+++ test_hashtable.py
@@ -1,4 +1,5 @@
-from hashtable import HashTable
+import pytest
+from hashtable import HashTable, BLANK
 
 """
 Test case should be as independent and atomic as possible
@@ -19,4 +20,28 @@ def test_should_report_capacity():
     assert len(HashTable(capacity=10)) == 10
 
 def test_should_create_empty_value_slots():
-    assert HashTable(capacity=3).values == [None, None, None]
+    assert HashTable(capacity=3).values == [BLANK, BLANK, BLANK]
+
+def test_should_insert_key_value_pairs():
+    hashtable = HashTable(capacity=100)
+
+    hashtable['hola'] = "Hello"
+    hashtable[98.2] = 39
+    hashtable[False] = True
+
+    assert 'Hello' in hashtable.values
+    assert 39 in hashtable.values
+    assert True in hashtable.values
+    assert len(hashtable) == 100
+
+def test_should_not_contain_none_value_when_created():
+    assert None not in HashTable(capacity=10).values
+
+def test_should_insert_none_value():
+    hashtable = HashTable(capacity=10)
+    hashtable['k'] = None
+    assert None in hashtable.values
+
+@pytest.mark.skip
+def test_should_not_shrink_when_removing_elements():
+    pass
